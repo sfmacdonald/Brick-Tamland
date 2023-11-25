880de3bfc62d427adf31c84542f810e5562bad3a
sfmacdonald <seanfmacdonald@icloud.com>
2023-11-24 19:04:45 -0700

    Updated code for css and js

--- README.md
+++ README.md
@@ -18,17 +18,17 @@ The purpose of the website is to view the current weather and 5-day forecast at 
 
 This web-based weather app has the following features and functions:
 
-Leverages 
-
-1. 
-    - 
-2. 
-    - 
-3. 
-4. 
-5. 
+1. Search, retrieve & display results leveraging OpenWeatherMap API [api.openweathermap.org]for a location's weather including:
+    - current weather and 5-day forecast 
+    - date
+    - weather icon
+    - temperature
+    - wind speed
+    - humidity.
+2. Maintain a search history list that
+    - Adds the searched city to the history when a new search is performed.
+    - Allow users to click on a city in the search history to view its current and future conditions.
   
-
 ## Usage
 
 The github repository may be found at https://github.com/sfmacdonald/Brick-Tamland
@@ -78,3 +78,8 @@ WHEN I view future weather conditions for that city
 THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
+
+API key: 2f5fee7f8b7747f31078b723051d688b
+- Please, use the endpoint api.openweathermap.org for your API calls
+- Example of API call:
+api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2f5fee7f8b7747f31078b723051d688b
---
+++ assets/js/script.js
@@ -0,0 +1,92 @@
+var apiKey = '2f5fee7f8b7747f31078b723051d688b';
+var apiUrl = 'https://api.openweathermap.org/data/2.5';
+
+$(document).ready(function () {
+  $('#search-form').on('submit', function (event) {
+    event.preventDefault();
+    searchWeather();
+  });
+
+  $('#history-list').on('click', 'li', function () {
+    var cityName = $(this).text();
+    getWeather(cityName);
+  });
+
+  // Load search history from local storage
+  var searchHistory = new Set(JSON.parse(localStorage.getItem('searchHistory')) || []);
+  var historyList = $('#history-list');
+
+  for (var cityName of searchHistory) {
+    var listItem = $('<li class="button"></li>').text(cityName);
+    historyList.append(listItem);
+  }
+});
+
+function searchWeather() {
+  var cityName = $('#city-input').val().trim();
+
+  if (cityName !== '') {
+    getWeather(cityName);
+    $('#city-input').val('');
+  }
+}
+
+async function getWeather(cityName) {
+  try {
+    var currentWeather = await $.getJSON(`${apiUrl}/weather?q=${cityName}&appid=${apiKey}&units=metric`);
+    var forecast = await $.getJSON(`${apiUrl}/forecast?q=${cityName}&appid=${apiKey}&units=metric`);
+
+    displayCurrentWeather(currentWeather);
+    displayForecast(forecast);
+    addToSearchHistory(cityName);
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+  }
+}
+
+function displayCurrentWeather(data) {
+  var tempInFahrenheit = (data.main.temp * 9/5) + 32;
+  var iconUrl = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
+  $('#current-weather').html(`
+    <h2>${data.name}, ${data.sys.country}</h2>
+    <p>Date: ${new Date(data.dt * 1000).toLocaleDateString()}</p>
+    <img src="${iconUrl}" alt="${data.weather[0].description} Icon">
+    <p>Temperature: ${tempInFahrenheit.toFixed(2)}°F</p>
+    <p>Humidity: ${data.main.humidity}%</p>
+    <p>Wind Speed: ${data.wind.speed} m/s</p>
+  `);
+}
+
+function displayForecast(data) {
+  var forecastDiv = $('#forecast').empty();
+
+  for (let i = 0; i < data.list.length; i += 8) {
+    var dayData = data.list[i];
+    var tempInFahrenheit = (dayData.main.temp * 9/5) + 32;
+
+    var forecastDayDiv = $('<div class="forecast-day"></div>').html(`
+      <p>Date: ${new Date(dayData.dt * 1000).toLocaleDateString()}</p>
+      <p>Temperature: ${tempInFahrenheit.toFixed(2)}°F</p>
+      <p>Humidity: ${dayData.main.humidity}%</p>
+      <p>Wind Speed: ${dayData.wind.speed} m/s</p>
+    `);
+    forecastDiv.append(forecastDayDiv);
+  }
+}
+
+function addToSearchHistory(cityName) {
+  var historyList = $('#history-list');
+
+  // Update the Set and local storage
+  var searchHistory = new Set(JSON.parse(localStorage.getItem('searchHistory')) || []);
+  searchHistory.add(cityName);
+  localStorage.setItem('searchHistory', JSON.stringify(Array.from(searchHistory)));
+
+  // Clear and re-populate the history list
+  historyList.empty();
+  for (var cityName of searchHistory) {
+    var listItem = $('<li class="button"></li>').text(cityName);
+    historyList.append(listItem);
+  }
+}
